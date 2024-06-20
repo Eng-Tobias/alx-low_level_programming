8b35f234de61930112ca844e9447124906321f46
Eng-Tobias <engtobiasguya100@gmail.com>
2024-06-20 14:35:50 -0700

    Fix syntax errors and string concatenation in 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
 int main(void)
 {
 /* Use the write system call to print to stderr */
-const char *quote = "and that piece of art is useful\" - "Dora Korpar, 2015-10-19\n";
+const char *quote = "and that piece of art is useful\" - "
+"Dora Korpar, 2015-10-19\n";
 write(STDERR_FILENO, quote, 63);
 
 return (1);
