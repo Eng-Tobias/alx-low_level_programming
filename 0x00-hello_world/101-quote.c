a2c57b29dbec25bf515ee430e68fe66406c5f19b
Eng-Tobias <engtobiasguya100@gmail.com>
2024-06-20 14:19:57 -0700

    Address line length warning in 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,9 @@
 int main(void)
 {
 /* Use the write system call to print to stderr */
-const char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, quote, 59);
+const char *quote = "and that piece of art is useful\" - "
+"Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, quote, 63);
 
 return (1);
 }
