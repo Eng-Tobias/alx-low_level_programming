dac326c8c5bcd15f9c7871df3fe81e08f0c09c06
Eng-Tobias <engtobiasguya100@gmail.com>
2024-06-26 02:41:32 -0700

    Fix Betty style issues in print_to_98 function

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,17 +11,18 @@ void print_to_98(int n)
 {
 int i;
 
-if (n <= 98) 
+if (n <= 98)
 {
-for (i = n; i <= 98; i++) 
+for (i = n; i <= 98; i++)
 {
 printf("%d", i);
 if (i != 98)
 printf(", ");
 }
-} else 
+} 
+else
 {
-for (i = n; i >= 98; i--) 
+for (i = n; i >= 98; i--)
 {
 printf("%d", i);
 if (i != 98)
