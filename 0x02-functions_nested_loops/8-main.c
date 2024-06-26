e9a2347cff76f0dbc0e7cb47ef78111f1a4e3038
Eng-Tobias <engtobiasguya100@gmail.com>
2024-06-26 02:45:33 -0700

    Fix Betty style issues in print_to_98 function

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,14 @@
+/* 8-main.c */
+
+#include "main.h"
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+jack_bauer();
+return (0);
+}
---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,14 @@
+/* 9-main.c */
+
+#include "main.h"
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+times_table();
+return (0);
+}
