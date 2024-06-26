a9068f0358b6cfc669aae2e297e77efcc1d20204
Eng-Tobias <engtobiasguya100@gmail.com>
2024-06-26 04:57:12 -0700

    Add program to compute sum of even Fibonacci numbers up to 4,000,000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* main - computes and prints the sum of even Fibonacci numbers
+*        whose values do not exceed 4,000,000
+* 
+* Return: Always 0
+*/
+int main(void)
+{
+unsigned long int a = 1, b = 2, next, sum = 0;
+
+while (b <= 4000000)
+{
+if (b % 2 == 0)
+{
+sum += b;
+}
+next = a + b;
+a = b;
+b = next;
+}
+
+_putchar((sum / 1000000) + '0');
+sum %= 1000000;
+_putchar((sum / 100000) + '0');
+sum %= 100000;
+_putchar((sum / 10000) + '0');
+sum %= 10000;
+_putchar((sum / 1000) + '0');
+sum %= 1000;
+_putchar((sum / 100) + '0');
+sum %= 100;
+_putchar((sum / 10) + '0');
+sum %= 10;
+_putchar(sum + '0');
+_putchar('\n');
+
+return (0);
+}
