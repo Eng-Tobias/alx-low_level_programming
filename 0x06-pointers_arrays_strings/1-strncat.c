cad5a3945e886424504e85016cdb4fd3ee7e2e07
Eng-Tobias <engtobiasguya100@gmail.com>
2024-07-04 18:24:40 -0700

    Implement _strncat function

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* _strncat - Concatenates two strings.
+* @dest: The destination string.
+* @src: The source string.
+* @n: The maximum number of bytes to be used from src.
+*
+* Return: A pointer to the resulting string dest.
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int dest_len = 0;
+int i = 0;
+
+/* Find the end of dest */
+while (dest[dest_len] != '\0')
+{
+dest_len++;
+}
+
+/* Append src to dest*/
+while (i < n && src[i] != '\0')
+{
+dest[dest_len] = src[i];
+dest_len++;
+i++;
+}
+
+/* Null-terminate dest */
+dest[dest_len] = '\0';
+
+return dest;
+}
