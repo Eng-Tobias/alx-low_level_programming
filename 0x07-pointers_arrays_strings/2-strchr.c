e3600e94f8a8f283fd98c46a103a2903a88b4104
Eng-Tobias <engtobiasguya100@gmail.com>
2024-07-08 18:28:12 -0700

    Implement and test the _strchr function

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,12 +2,12 @@
 #include <stddef.h>
 
 /**
- * _strchr - locates a character in a string
- * @s: the string to search
- * @c: the character to find
- *
- * Return: pointer to the first occurrence of c in s, or NULL if not found
- */
+* _strchr - locates a character in a string
+* @s: the string to search
+* @c: the character to find
+*
+* Return: pointer to the first occurrence of c in s, or NULL if not found
+*/
 char *_strchr(char *s, char c)
 {
 while (*s)
