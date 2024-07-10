b089530dbe9526762fe3e64b118fd191a72a1dc2
Eng-Tobias <engtobiasguya100@gmail.com>
2024-07-10 06:13:24 -0700

    Correction: a function that returns the value of x raised to the power of y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
-* _pow_recursion - Returns the value of x raised to the power of y 
-* @x: The base number
-* @y: The exponent
+* _pow_recursion - Returns the value of x raised to the power of y
+* @x: The base value
+* @y: The exponent value
 *
-* Return: The result of x raised to the power of y
-*  -1 if y is lower than 0 (negative exponent not handled)
+* Return: The value of x raised to the power of y, or -1 if y is less than 0
 */
 int _pow_recursion(int x, int y)
 {
 if (y < 0)
-return (-1); /* Negative exponent not handled, return -1 as error */
-
+return (-1);
 if (y == 0)
-return (1); /* Base case: any number raised to the power of 0 is 1 */
-
-return (x) * _pow_recursion(x, y - 1); /* Recursive case: multiply x with(x^(y-1)) */
+return (1);
+return (x * _pow_recursion(x, y - 1));
 }
