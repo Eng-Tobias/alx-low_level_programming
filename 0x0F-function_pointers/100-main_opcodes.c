e89c075fe3119d8d9da6fc979363dc294074f4d4
Eng-Tobias <engtobiasguya100@gmail.com>
2024-07-24 19:48:32 -0700

    Add program to print opcodes of its own main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints the opcodes of its own main function
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+int num_bytes, i;
+unsigned char *main_ptr;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+
+num_bytes = atoi(argv[1]);
+
+if (num_bytes < 0)
+{
+printf("Error\n");
+return (2);
+}
+
+main_ptr = (unsigned char *)main;
+
+for (i = 0; i < num_bytes; i++)
+{
+printf("%02x", main_ptr[i]);
+if (i < num_bytes - 1)
+printf(" ");
+}
+
+printf("\n");
+return (0);
+}
