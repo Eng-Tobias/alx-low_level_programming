0ac5b2f936cd3e3360030e3e7b9b725ac2f3f0aa
Eng-Tobias <engtobiasguya100@gmail.com>
2024-07-24 19:42:46 -0700

    Add calculator program

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include "3-calc.h"
+
+/**
+* op_add - returns the sum of a and b
+* @a: first integer
+* @b: second integer
+* Return: sum of a and b
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+* op_sub - returns the difference of a and b
+* @a: first integer
+* @b: second integer
+* Return: difference of a and b
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+* op_mul - returns the product of a and b
+* @a: first integer
+* @b: second integer
+* Return: product of a and b
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+* op_div - returns the result of the division of a by b
+* @a: first integer
+* @b: second integer
+* Return: result of the division of a by b
+*/
+int op_div(int a, int b)
+{
+if (b == 0)
+{
+return (-1);  /* Return -1 for division by zero */
+}
+return (a / b);
+}
+
+/**
+* op_mod - returns the remainder of the division of a by b
+* @a: first integer
+* @b: second integer
+* Return: remainder of the division of a by b
+*/
+int op_mod(int a, int b)
+{
+if (b == 0)
+{
+return (-1);  /* Return -1 for modulo by zero */
+}
+return (a % b);
+}
