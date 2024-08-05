1c1a5da81a86efa103ec63a30f56b4353e1502a4
Eng-Tobias <engtobiasguya100@gmail.com>
2024-08-05 13:53:06 -0700

    Implement listint_len function

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 
 /**
-* struct listint_s - singly linked list
+* struct listint_s - singly linked list 
 * @n: integer
 * @next: points to the next node
 *
@@ -16,6 +16,6 @@ int n;
 struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif /* LISTS_H */
