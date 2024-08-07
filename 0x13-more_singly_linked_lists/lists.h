f1f57a4bb7b01da5699cad3a881f65a88c292d04
Eng-Tobias <engtobiasguya100@gmail.com>
2024-08-06 18:58:53 -0700

    Implement reverse_listint function and add corresponding test file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -28,5 +28,6 @@ int sum_listint(listint_t *head);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
 
 #endif /* LISTS_H */
