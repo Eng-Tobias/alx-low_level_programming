1e520bbaac828a4ada10733ef2081c2b68ba1b1b
Eng-Tobias <engtobiasguya100@gmail.com>
2024-08-06 18:18:19 -0700

    Add 6-main.c to test pop_listint function

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -22,5 +22,6 @@ listint_t *add_nodeint(listint_t **head, const int n);
 size_t print_listint(const listint_t *h);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
 
 #endif /* LISTS_H */
