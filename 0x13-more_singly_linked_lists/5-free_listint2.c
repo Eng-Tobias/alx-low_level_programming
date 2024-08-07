0e9cd67436ce77238fdc6f352451f6b6334053e9
Eng-Tobias <engtobiasguya100@gmail.com>
2024-08-06 18:08:41 -0700

    Add free_listint2 function to free list and set head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - Frees a listint_t list and sets the head to NULL.
+ * @head: Double pointer to the head of the list.
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *temp;
+if (head == NULL)
+return;
+while (*head != NULL)
+{
+temp = *head;
+*head = (*head)->next;
+free(temp);
+}
+*head = NULL;
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,5 +21,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 listint_t *add_nodeint(listint_t **head, const int n);
 size_t print_listint(const listint_t *h);
 void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
 
 #endif /* LISTS_H */
