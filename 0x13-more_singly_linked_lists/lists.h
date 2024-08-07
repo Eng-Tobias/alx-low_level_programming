9e502613b4d0a6799ae6ea5fbc0f2953b3edcc33
Eng-Tobias <engtobiasguya100@gmail.com>
2024-08-06 18:03:16 -0700

    Add function to free a listint_t list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -20,5 +20,6 @@ size_t listint_len(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 listint_t *add_nodeint(listint_t **head, const int n);
 size_t print_listint(const listint_t *h);
+void free_listint(listint_t *head);
 
 #endif /* LISTS_H */
