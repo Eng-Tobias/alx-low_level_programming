e7f64025f23705241329da14eae329992ca091d2
Eng-Tobias <engtobiasguya100@gmail.com>
2024-08-06 19:36:37 -0700

    Implement function to find loop in linked list and update header file

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+* find_listint_loop - Finds the loop in a listint_t linked list.
+* @head: Pointer to the head of the list.
+*
+* Return: The address of the node, or NULL if there is no loop.
+*/
+listint_t *find_listint_loop(listint_t *head)
+{
+listint_t *slow=head,*fast=head;
+while(slow&&fast&&fast->next)
+{
+slow=slow->next;
+fast=fast->next->next;
+if(slow==fast)
+{
+slow=head;
+while(slow!=fast)
+{
+slow=slow->next;
+fast=fast->next;
+}
+return(slow);
+}
+}
+return(NULL);
+}
