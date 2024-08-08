37052b762a88d9dfa90c056eb4e2c7c1166f88d9
Eng-Tobias <engtobiasguya100@gmail.com>
2024-08-08 13:03:05 -0700

    Implement clear_bit function to set a bit to 0 at a given index

--- 0x14-bit_manipulation/4-main.c
+++ 0x14-bit_manipulation/4-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
 * main - check the code
 *
@@ -8,14 +9,15 @@
 int main(void)
 {
 unsigned long int n;
+
 n = 1024;
-set_bit(&n, 5);
+clear_bit(&n, 10);
 printf("%lu\n", n);
 n = 0;
-set_bit(&n, 10);
+clear_bit(&n, 10);
 printf("%lu\n", n);
 n = 98;
-set_bit(&n, 0);
+clear_bit(&n, 1);
 printf("%lu\n", n);
 return (0);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 void print_binary(unsigned long int n);
 int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 #endif /* MAIN_H */
 
