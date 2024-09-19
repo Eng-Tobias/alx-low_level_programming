199a0c0a21133144da07e2dc719a84e3f380b8c5
Eng-Tobias <engtobiasguya100@gmail.com>
2024-09-19 13:42:15 -0700

    Add function to free a doubly linked list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Returns the number of elements in a doubly linked dlistint_t list
- * @h: Pointer to the head of the doubly linked list
- *
- * Return: The number of elements in the list
- */
+* dlistint_len - Returns the number of elements in a doubly linked dlistint_t list
+* @h: Pointer to the head of the doubly linked list
+*
+* Return: The number of elements in the list
+*/
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t count = 0;
+size_t count = 0;
 
-    while (h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
 
-    return count;
+return count;
 }
