73f5c94db8c8003e37a9662fe487fc63058d4f48
Eng-Tobias <engtobiasguya100@gmail.com>
2024-09-27 14:03:27 -0700

    Add dynamic library and code injection script

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp /path/to/gm .   # Replace with the actual path to the gm binary
+export LD_PRELOAD="./your_custom_library.so"  # Replace with your custom library
---
+++ 0x18-dynamic_libraries/101-win.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int n1 = 9;
+int n2 = 8;
+int n3 = 10;
+int n4 = 24;
+int n5 = 75;
+int bonus = 9;
+
+int main() {
+    return 0;
+}
