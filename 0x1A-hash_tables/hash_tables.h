35432d8da24402be01fe2da549436c6d66bcdb0d
Eng-Tobias <engtobiasguya100@gmail.com>
2024-10-11 18:38:28 -0700

    Implement tasks 0 to 6 for hash table project

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -2,74 +2,31 @@
 #define HASH_TABLES_H
 
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
-/**
- * struct hash_node_s - Node of a hash table
- * @key: The key, string
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the list
- */
+/* Data structure definitions */
 typedef struct hash_node_s
 {
-    char *key;
-    char *value;
-    struct hash_node_s *next;
+    char *key;                /* The key, string */
+    char *value;              /* The value corresponding to a key */
+    struct hash_node_s *next; /* Pointer to the next node of the list */
 } hash_node_t;
 
-/**
- * struct hash_table_s - Hash table
- * @size: The size of the array
- * @array: An array of pointers to the nodes
- */
 typedef struct hash_table_s
 {
-    unsigned long int size;
-    hash_node_t **array;
+    unsigned long int size;    /* The size of the array */
+    hash_node_t **array;       /* An array of size @size, pointer to linked list */
 } hash_table_t;
-
-/**
- * struct shash_node_s - Sorted node of a hash table
- * @key: The key, string
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the list
- * @sprev: A pointer to the previous sorted node
- * @snext: A pointer to the next sorted node
- */
-typedef struct shash_node_s
-{
-    char *key;
-    char *value;
-    struct shash_node_s *next;
-    struct shash_node_s *sprev;
-    struct shash_node_s *snext;
-} shash_node_t;
-
-/**
- * struct shash_table_s - Sorted hash table
- * @size: The size of the array
- * @array: An array of pointers to the nodes
- * @shead: A pointer to the first sorted node
- * @stail: A pointer to the last sorted node
- */
-typedef struct shash_table_s
-{
-    unsigned long int size;
-    shash_node_t **array;
-    shash_node_t *shead;
-    shash_node_t *stail;
-} shash_table_t;
 
 /* Function prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-shash_table_t *shash_table_create(unsigned long int size);
-int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-char *shash_table_get(const shash_table_t *ht, const char *key);
-void shash_table_print(const shash_table_t *ht);
-void shash_table_print_rev(const shash_table_t *ht);
-void shash_table_delete(shash_table_t *ht);
 
 #endif /* HASH_TABLES_H */
+
